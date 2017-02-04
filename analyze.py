f9db66456b835e5892380b4e5e0e9af0b0a8637b
Nicolas Babault <nikobab@hotmail.com>
2017-02-04 16:49:13 -0500

    analyze new

--- analyze.py
+++ analyze.py
@@ -1,12 +1,5 @@
 import sqlite3
 import csv
-connection = sqlite3.connect(":memory:")
-cursor = connection.cursor()
-cursor.execute("SELECT TAble t (name,age);")
-with open('data.csv','rb') as fin:
- dr = csv.DictReader(fin)
- to_db = [(import sqlite3
-import csv
 
 connection = sqlite3.connect(":memory:")
 cursor = connection.cursor()
@@ -21,11 +14,6 @@ connection.commit()
 
 cursor = connection.cursor()
 cursor.execute("SELECT * FROM t;")
-results = cursor.fetchall()
-for r in results:
- print(r)
-cursor.close()
-connection.close()
 results = cursor.fetchall()
 for r in results:
  print(r)
